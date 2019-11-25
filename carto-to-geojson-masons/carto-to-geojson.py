54f25949de96fc21a11e7485baa3ddc937994ac5
Garrett Dash Nelson <garrettdashnelson@gmail.com>
2019-11-25 12:27:06 -0500

    running after calvin done with crosswalks

--- carto-to-geojson-masons/carto-to-geojson.py
+++ carto-to-geojson-masons/carto-to-geojson.py
@@ -10,6 +10,9 @@ with open('crosswalks.csv','r+') as crosswalksRaw:
 
 	for row in crosswalks:
 
+		if len(row['carto_table_name'].strip(' ')) < 1: continue
+		print("Downloading table {}".format(row['carto_table_name']))
+
 		apiUrl = "https://bpl-maps.carto.com/api/v2/sql?q=select * from {}&format=GeoJSON".format(row['carto_table_name'])
 
 		r = requests.get(apiUrl)
@@ -17,7 +20,7 @@ with open('crosswalks.csv','r+') as crosswalksRaw:
 		j = r.json()
 
 
-
+		print("Adding {} features".format(len(j['features'])))
 		for feature in j['features']:
 
 			featuresList.append({'type': 'Feature', 'geometry': feature['geometry'], 'properties': {'name': feature['properties']['name'], 'folder': row['canonical_folder_name']}})
